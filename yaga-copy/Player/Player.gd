f8906b119bb70fe708dabf0c819d318650acd6f9
2BytesGoat <2bytesgoat@gmail.com>
2021-01-24 00:10:05 +0200

    Working on animation tree

--- yaga-copy/Player/Player.gd
+++ yaga-copy/Player/Player.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 200
-const ACCELERATION = 400
+const MAX_SPEED = 100
+const ACCELERATION = 200
 const FRICTION = 1250
 
 var velocity = Vector2.ZERO
+var is_running = false
+
+onready var spritePlayer = $SpritePlayer
+onready var animation = $AnimationPlayer
+onready var animationTree = $AnimationTree
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,10 +19,18 @@ func _physics_process(delta):
 				Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	if input_vector != Vector2.ZERO:
+	is_running = input_vector != Vector2.ZERO
+	if is_running:
+		if input_vector.x > 0:
+			spritePlayer.scale.x = 1
+		else:
+			spritePlayer.scale.x = -1
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, 
 				ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
+		
+	animationTree["parameters/conditions/isIdle"] = not is_running
+	animationTree["parameters/conditions/isRunning"] = is_running
+	
 	velocity = move_and_slide(velocity)
