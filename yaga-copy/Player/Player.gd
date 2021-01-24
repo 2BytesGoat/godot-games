ba3e747abe9d8ff10be26a35b003fd91d81146be
2BytesGoat <2bytesgoat@gmail.com>
2021-01-24 18:32:45 +0200

    Add projectile sprites

--- yaga-copy/Player/Player.gd
+++ yaga-copy/Player/Player.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
 const MAX_SPEED = 75
-const ACCELERATION = 150
+const ACCELERATION = 500
 const FRICTION = 500
+
+const arrowProjectile = preload("res://Projectiles/Arrow.tscn")
 
 enum{
 	MOVE,
@@ -10,13 +12,18 @@ enum{
 }
 
 var velocity = Vector2.ZERO
+var flip_sprite_player = false
 var state = MOVE
 
 onready var spritePlayer = $SpritePlayer
+onready var cursorPlayer = $CursorPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var shootArrowPosition = $SpritePlayer/ArrowSpawnPosition
 
 func _physics_process(delta):
+	var mouse_pos = get_global_mouse_position()
+	cursorPlayer.look_at(mouse_pos)
 	match state:
 		MOVE:
 			move_state(delta)
@@ -28,12 +35,12 @@ func shoot_state(_delta):
 	animationState.travel("ShootArrow")
 	
 func shoot_animation_finished():
+	var arrow = arrowProjectile.instance()
+	shootArrowPosition.add_child(arrow)
+	arrow.launch(0.5)
 	state = MOVE
 
 func move_state(delta):
-	var mouse_position = get_local_mouse_position()
-	spritePlayer.set_flip_h(mouse_position.x > 0) # flip sprite
-	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - \
 				Input.get_action_strength("ui_left")
@@ -41,12 +48,9 @@ func move_state(delta):
 				Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	var shoot_arror = Input.is_action_just_pressed("hero_shoot")
-	
-	if shoot_arror:
-		state = SHOOT
-	
 	if input_vector != Vector2.ZERO:
+		flip_sprite_player = input_vector.x > 0
+		spritePlayer.set_flip_h(flip_sprite_player) # flip sprite
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, 
 				ACCELERATION * delta)
 		animationState.travel("Run")
@@ -54,3 +58,8 @@ func move_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		animationState.travel("Idle")
 	velocity = move_and_slide(velocity)
+
+	if Input.is_action_just_pressed("hero_shoot"):
+		flip_sprite_player = get_local_mouse_position().x > 0
+		spritePlayer.set_flip_h(flip_sprite_player) # flip sprite
+		state = SHOOT
