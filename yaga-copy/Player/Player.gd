6856b3b7985655c622ced9ade43e43e1d9fb3c40
2BytesGoat <2bytesgoat@gmail.com>
2021-01-24 13:52:14 +0200

    Add attack animation

--- yaga-copy/Player/Player.gd
+++ yaga-copy/Player/Player.gd
@@ -1,19 +1,39 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 50
+const MAX_SPEED = 75
 const ACCELERATION = 150
 const FRICTION = 500
 
+enum{
+	MOVE,
+	SHOOT
+}
+
 var velocity = Vector2.ZERO
-var sprite_orientation = 1
-var is_running = false
+var state = MOVE
 
 onready var spritePlayer = $SpritePlayer
-
-onready var animation = $AnimationPlayer
 onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		SHOOT:
+			shoot_state(delta)
+	
+func shoot_state(_delta):
+	velocity = Vector2.ZERO
+	animationState.travel("ShootArrow")
+	
+func shoot_animation_finished():
+	state = MOVE
+
+func move_state(delta):
+	var mouse_position = get_local_mouse_position()
+	spritePlayer.set_flip_h(mouse_position.x > 0) # flip sprite
+	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - \
 				Input.get_action_strength("ui_left")
@@ -21,19 +41,16 @@ func _physics_process(delta):
 				Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
-	is_running = input_vector != Vector2.ZERO
-	if is_running:
-		if input_vector.x > 0:
-			sprite_orientation = 1
-		elif input_vector.x < 0:
-			sprite_orientation = -1
-		spritePlayer.scale.x = sprite_orientation
+	var shoot_arror = Input.is_action_just_pressed("hero_shoot")
+	
+	if shoot_arror:
+		state = SHOOT
+	
+	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, 
 				ACCELERATION * delta)
+		animationState.travel("Run")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
-	animationTree["parameters/conditions/isIdle"] = not is_running
-	animationTree["parameters/conditions/isRunning"] = is_running
-	
+		animationState.travel("Idle")
 	velocity = move_and_slide(velocity)
