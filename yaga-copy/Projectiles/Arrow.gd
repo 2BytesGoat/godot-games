eab5ba9610633337d40109df72c0bb824343022d
2BytesGoat <2bytesgoat@gmail.com>
2021-01-28 22:37:22 +0200

    Add cats

--- yaga-copy/Projectiles/Arrow.gd
+++ yaga-copy/Projectiles/Arrow.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 var launched = false
+var collided = false
 var velocity = Vector2.ZERO
 
 var mass = 0.25
 
 func _physics_process(delta):
-	if launched:
+	if launched and not collided:
 		position += velocity * delta
 		rotation = velocity.angle()
+		collided = len(get_overlapping_bodies()) > 0
+	if collided: # make arrows stick
+		get_parent().remove_child(self)
 		
 func launch(rot, speed):
 	launched = true
