96d5167302346105154a06665d51a124b7b30a24
2BytesGoat <2bytesgoat@gmail.com>
2021-02-04 21:29:00 +0200

    Add hitbox and hurtbox

--- hearthbeast_with_a_twist/World/Grass.gd
+++ hearthbeast_with_a_twist/World/Grass.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("hero_attack"):
-		var GrassEffect = load("res://World/GrassEffect.tscn")
-		var grass_effect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grass_effect)
-		grass_effect.global_position = global_position
-		queue_free()
+func create_grass():
+	var GrassEffect = load("res://World/GrassEffect.tscn")
+	var grass_effect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grass_effect)
+	grass_effect.global_position = global_position
+
+func _on_HurtBox_area_entered(area):
+	create_grass()
+	queue_free()
