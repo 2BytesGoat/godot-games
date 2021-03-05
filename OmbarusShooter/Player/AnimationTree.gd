6a8f484a81aacbbdb66def807792603dd00d4cf9
2BytesGoat <2bytesgoat@gmail.com>
2021-03-05 08:00:53 +0200

    Cleaned up animation code

--- OmbarusShooter/Player/AnimationTree.gd
+++ OmbarusShooter/Player/AnimationTree.gd
@@ -1,10 +1,26 @@
 extends AnimationTree
 
 var blend_position = Vector2.ZERO
-var input_vector = Vector3.ZERO
+
+onready var animation_playback = get("parameters/playback")
 
 func _ready():
 	pass # Replace with function body.
 
-func update_animation(animation_name, input_vector):
-	pass
+func update_animation(input_vector:Vector3, is_running:bool):
+	# move animation related code in a AnimationTree script
+	# for smooth animtions use velocity instead of input_vector
+	if input_vector != Vector3.ZERO:
+		if input_vector.z >= 0:
+			blend_position = Vector2(input_vector.x, input_vector.z)
+		else:
+			blend_position = Vector2(-input_vector.x, input_vector.z)
+		set("parameters/Walking/blend_position", blend_position)
+		set("parameters/Running/blend_position", blend_position)
+		
+		if is_running and input_vector.z > 0:
+			animation_playback.travel('Running')
+		else:
+			animation_playback.travel('Walking')
+	else:
+		animation_playback.travel('Idle')
--- OmbarusShooter/Player/Player.gd
+++ OmbarusShooter/Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody
 
 export var MAX_WALK_SPEED = 2
-export var MAX_RUN_SPEED = 4.5
+export var MAX_RUN_SPEED = 5
 export var ACCELERATION = 30
 export var FRICTION = 500
 
@@ -13,6 +13,8 @@ var velocity = Vector3.ZERO
 var input_vector = Vector3.ZERO
 var animation_transitionx = 20
 
+var is_running = false
+var target_speed = Vector3.ZERO
 var blend_position = Vector2.ZERO
 
 func _ready():
@@ -21,9 +23,7 @@ func _ready():
 func _physics_process(delta):
 	#var root_motion: Transform = animation_tree.get_root_motion_transform()
 	#var v = root_motion.origin / delta
-	#print(v)
 	move_state(delta)
-	
 	
 func move_state(delta):
 	input_vector.x = Input.get_action_strength("move_left") - \
@@ -32,30 +32,18 @@ func move_state(delta):
 				Input.get_action_strength("move_backward")
 	input_vector = input_vector.normalized()
 	
-	var is_running = Input.is_action_pressed("move_faster")
+	is_running = Input.is_action_pressed("move_faster")
 	
 	if input_vector != Vector3.ZERO:
-		if is_running and input_vector.z > 0:
-			velocity = velocity.move_toward(input_vector * MAX_RUN_SPEED, 
-						ACCELERATION * delta)
-			animation_playback.travel('Running')
-		else:
-			velocity = velocity.move_toward(input_vector * MAX_WALK_SPEED, 
-							ACCELERATION * delta)
-			# move animation related code in a AnimationTree script
-			# for smooth animtions use velocity instead of input_vector
-			if input_vector.z >= 0:
-				blend_position = Vector2(input_vector.x, input_vector.z)
-			else:
-				blend_position = Vector2(-input_vector.x, input_vector.z)
-			
-			animation_tree.set("parameters/Walking/blend_position", blend_position)
-			animation_playback.travel('Walking')
+		target_speed = input_vector * MAX_RUN_SPEED if is_running else \
+				input_vector * MAX_WALK_SPEED
 		
+		velocity = velocity.move_toward(target_speed, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector3.ZERO, FRICTION * delta)
-		animation_playback.travel('Idle')
-
+		
+	animation_tree.update_animation(input_vector, is_running)
+		
 	velocity.y = 0 if is_on_floor() else velocity.y - 9.8 
-
 	move_and_slide(velocity, Vector3.UP)
+	
