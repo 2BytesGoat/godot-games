0b161974d57f3a28d3f9f12826680ec377c62fda
2BytesGoat <2bytesgoat@gmail.com>
2021-03-06 10:11:56 +0200

    Hide the evidence

--- _archive/_unfinished/OmbarusShooter/Player/Player.gd
+++ _archive/_unfinished/OmbarusShooter/Player/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody
 
+export var MOUSE_SENSITIVITY = 0.1
 export var MAX_WALK_SPEED = 2
 export var MAX_RUN_SPEED = 5
 export var ACCELERATION = 30
 export var FRICTION = 500
 
+onready var player_camera: Camera = $PlayerRotPivot/Camera
 export(NodePath) onready var animation_tree = get_node(animation_tree)
 onready var animation_playback = animation_tree.get("parameters/playback")
 
@@ -18,12 +20,26 @@ var target_speed = Vector3.ZERO
 var blend_position = Vector2.ZERO
 
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if event is InputEventMouseMotion:
+		aim_camera(event)
 
 func _physics_process(delta):
 	#var root_motion: Transform = animation_tree.get_root_motion_transform()
 	#var v = root_motion.origin / delta
 	move_state(delta)
+	
+func aim_camera(event):
+	# rotate player
+	rotate_y(deg2rad(-event.relative.x * MOUSE_SENSITIVITY))
+	# rotate camera
+	player_camera.rotate_x(deg2rad(-event.relative.y * MOUSE_SENSITIVITY))
+	player_camera.rotation.x = clamp(player_camera.rotation.x, deg2rad(-70), deg2rad(70))
 	
 func move_state(delta):
 	input_vector.x = Input.get_action_strength("move_left") - \
@@ -42,8 +58,8 @@ func move_state(delta):
 	else:
 		velocity = velocity.move_toward(Vector3.ZERO, FRICTION * delta)
 		
-	animation_tree.update_animation(input_vector, is_running)
-		
 	velocity.y = 0 if is_on_floor() else velocity.y - 9.8 
 	move_and_slide(velocity, Vector3.UP)
 	
+	# ~~ START ANIMATION LOOP ~~
+	animation_tree.update_animation(input_vector, is_running)
