683932f6b82dfd309aad86d9f038ef9047a81633
2BytesGoat <2bytesgoat@gmail.com>
2021-03-07 15:42:02 +0200

    Add projectile push

--- IHasMagic/Spells/Projectile.gd
+++ IHasMagic/Spells/Projectile.gd
@@ -3,7 +3,11 @@ extends RigidBody
 export var travel_speed = 50
 export var lifetime = 1.5
 
+var launch_origin
 var death_timer
+
+func _ready():
+	launch_origin = global_transform.origin
 
 func launch():
 	death_timer = Timer.new()
@@ -13,3 +17,9 @@ func launch():
 	
 	set_as_toplevel(true)
 	apply_central_impulse(-transform.basis.z * travel_speed)
+
+func _on_Projectile_body_entered(body):
+	if body is KinematicBody:
+		var direction = body.transform.origin - launch_origin 
+		body.move_and_slide(direction.normalized() * travel_speed * 2, Vector3.UP)
+	queue_free()
