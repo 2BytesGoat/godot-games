bf124f661007ba158e8c3149f4ef16c5329d7463
2BytesGoat <2bytesgoat@gmail.com>
2021-03-18 07:57:29 +0200

    Add janky animations

--- IHasMagic/Player/Player.gd
+++ IHasMagic/Player/Player.gd
@@ -1,8 +1,14 @@
 extends KinematicBody
 
-const move_speed = 550
+const move_speed = 600
 const FRICTION = 25 # consider scaling friction with current health
 
+enum {
+	MOVE,
+	SHOOT
+}
+
+var state = MOVE
 var travel_path = []
 var travel_path_idx = 0
 var knockback_vector = Vector3.ZERO
@@ -12,6 +18,9 @@ onready var nav: Navigation = get_parent()
 onready var status = $Status
 onready var dir_sprite: Spatial = $DirectionSprite
 onready var projectile_spwan = $Weapon/Position3D
+onready var anim_tree = $AnimationTree
+
+onready var anim_state = anim_tree.get("parameters/playback")
 onready var Projectile = preload("res://Spells/Projectile.tscn")
 
 func _ready():
@@ -35,21 +44,27 @@ func cast_spell(target_pos):
 	new_projectile.launch()
 	
 func _physics_process(delta):
-	var global_pos = global_transform.origin
 
 	if travel_path_idx < travel_path.size():
-		var move_vec = (travel_path[travel_path_idx] - global_pos)
+		var move_vec = (travel_path[travel_path_idx] - global_transform.origin)
 		if move_vec.length() < 0.1:
 			travel_path_idx += 1
 		else:
 			update_orientation(travel_path[travel_path_idx])
 			movement = move_and_slide(move_vec.normalized() * move_speed * delta, Vector3.UP)
-			
-	if knockback_vector != Vector3.ZERO: #issues when colliding with grid map
+			anim_state.travel("Run_Forward")
+	else:
+		movement = move_and_slide(Vector3.ZERO, Vector3.UP)
+		anim_state.travel("Idle_1")
+		
+#	if anim_state.get_current_node() == "Idle_1":
+#		anim_tree.root_motion_track = "Armature/Skeleton:left_toe_end"
+#	else:
+#		anim_tree.root_motion_track = "Armature/Skeleton:root_motion"
+	
+	if knockback_vector != Vector3.ZERO:
 		knockback_vector = knockback_vector.move_toward(Vector3.ZERO, FRICTION * delta)
 		knockback_vector = move_and_slide(knockback_vector)
-	
-	movement += knockback_vector
 	
 func _on_Status_no_health():
 	queue_free()
